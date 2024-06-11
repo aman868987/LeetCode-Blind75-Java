28e65dbf1f77a41a9029c5b340286f5ce72107fd
aman868987 <ya.aman.ay@gmail.com>
2024-06-11 13:06:27 +0530

    217. Contains Duplicate - solution

---
+++ src/ContainsDuplicate_217.java
@@ -0,0 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate_217 {
+
+    //problem link : https://leetcode.com/problems/contains-duplicate/description/
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> set = new HashSet<>();
+        for (int i : nums) {
+            if (set.contains(i)) {
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+
+    }
+
+}
